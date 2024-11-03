26c95426a6a1358589f204ace43ccb8ebd673688
IMS212 <ims@irisshaders.dev>
2024-11-03 07:35:57 -0800

    Fix weather on 1.21.2

--- common/src/main/java/net/irisshaders/iris/mixin/MixinWeatherRenderer.java
+++ common/src/main/java/net/irisshaders/iris/mixin/MixinWeatherRenderer.java
@@ -1,8 +1,16 @@
 package net.irisshaders.iris.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.irisshaders.iris.Iris;
 import net.irisshaders.iris.pipeline.WorldRenderingPipeline;
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.WeatherEffectRenderer;
+import net.minecraft.server.level.ParticleStatus;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -16,5 +24,21 @@ public class MixinWeatherRenderer {
 		}
 
 		return depthMaskEnabled;
+	}
+
+	@WrapMethod(method = "render(Lnet/minecraft/world/level/Level;Lnet/minecraft/client/renderer/LightTexture;IFLnet/minecraft/world/phys/Vec3;)V")
+	private void iris$disableWeather(Level level, LightTexture lightTexture, int i, float f, Vec3 vec3, Operation<Void> original) {
+		if (Iris.getPipelineManager().getPipeline().map(WorldRenderingPipeline::shouldRenderWeather).orElse(true)) {
+			original.call(level, lightTexture, i, f, vec3);
+		}
+	}
+
+	@WrapMethod(method = "tickRainParticles")
+	private void disableRainParticles(ClientLevel clientLevel, Camera camera, int i, ParticleStatus particleStatus, Operation<Void> original) {
+		if (!Iris.getPipelineManager().getPipeline().map(WorldRenderingPipeline::shouldRenderWeatherParticles).orElse(true)) {
+			original.call(clientLevel, camera, i, ParticleStatus.MINIMAL);
+		} else {
+			original.call(clientLevel, camera, i, particleStatus);
+		}
 	}
 }
